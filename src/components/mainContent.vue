af155751e1c0bf728c6f5f64d3c1402f2c54c425
MikasaBili <980415069@qq.com>
2017-02-10 23:10:48 +0800

    dark
    supreme

---
+++ src/components/mainContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mainBackground mainCompose"></div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style lang="scss">
+    .mainBackground {
+        background-image:url('/static/img/background.jpg');
+    }
+    .mainCompose {
+        min-height: 100vh;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: 50%;
+    }
+</style>
